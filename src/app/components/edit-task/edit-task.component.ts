fa7202d6b9a2cc0bfc64f0b312b5c51ffdc29793
Maulid Abdullahi <maulidabdullahi3@gmail.com>
2021-06-22 10:54:40 +0300

    added edit functionality

--- src/app/components/edit-task/edit-task.component.ts
+++ src/app/components/edit-task/edit-task.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TaskService } from 'src/app/services/task.service';
+import { ToastrService } from 'ngx-toastr';
+import { Task } from '../../Task';
 
 
 @Component({
@@ -10,10 +12,14 @@ import { TaskService } from 'src/app/services/task.service';
   styleUrls: ['./edit-task.component.css'],
 })
 export class EditTaskComponent implements OnInit {
+  tasks: Task[] = [];
   EditTask: any;
   editData: any;
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
 
-  constructor(private fb: FormBuilder, private taskservice: TaskService, private router: ActivatedRoute) {}
+  constructor(private fb: FormBuilder, private taskservice: TaskService, private router: ActivatedRoute, private toastr: ToastrService) {}
   ngOnInit(): void {
     console.log('this.router.snapshot.params',this.router.snapshot.params.id)
     this.EditTask = this.fb.group({
@@ -28,7 +34,28 @@ export class EditTaskComponent implements OnInit {
       this.EditTask.controls['day'].setValue(this.editData.day);
       this.EditTask.controls['reminder'].setValue(this.editData.reminder);
     });
-  
   }
+
+  onEditSubmit(task: Task){
+    if (!this.text || !this.day) {
+      this.toastr.error('', 'please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+    this.taskservice.addTask(newTask).subscribe();
+    this.toastr.success('', 'Successfully added  task');
+
+    //clear inputs after submition
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+
+
+}
   
 }
